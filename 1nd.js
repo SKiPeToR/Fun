411dd26fa83f1c85d4d97a03a997bfa67b91ce78
SKiPeToR <49019994+SKiPeToR@users.noreply.github.com>
2019-05-26 18:22:33 +0300

    Update 1nd.js
    Исправил два задания

--- 1nd.js
+++ 1nd.js
@@ -11,6 +11,7 @@ function addMinutes(hours, minutes, addition){
     minutes+=addition;
     while (minutes>59) {hours+=1; minutes-=60}
     if (minutes<10) minutes='0'+minutes;
+    if (hours>=24) hours-=24;
     if (hours<10) hours='0'+hours;
     return hours+':'+minutes;
 }
@@ -21,7 +22,7 @@ function isPointInCircle(x,y) {
     return Math.pow(x-3,2)+Math.pow(y-5,2) <= 16;
 }
 function isPointInFigure(x,y) {
-    check=(x<=-5/3*y+5)&&(x>=7/4*x-7)&&(-12*x-96<=8*y)&&(x<=5/2*y+5);
+    check=(x<=-5/3*y+5)&&(x>=7/4*y-7)&&(-12*x-96<=8*y)&&(x<=5/2*y+5);
     return check;
 
 
